dfdf331b88dfdfdfbafb1b3020e8cbc50cf3141a
v.ITalik <vitalik.gricenko77@gmail.com>
2019-09-01 17:06:45 +0300

    The first version of the slider + js

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type = "button" class = "slick-prev"> <img    src="img/icons/left.png" alt="slick-prev"> </button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/right.png" alt="slick-next"></button>',
+        responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+          dots: true,
+          arrows: false,
+        }
+          }
+            ],
+      });
+  });
+
+
