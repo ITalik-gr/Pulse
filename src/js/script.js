1a8640fcb76e8813a8b580add2d5d756086bf7dd
v.ITalik <vitalik.gricenko77@gmail.com>
2019-09-14 21:52:58 +0300

    animate | wow.js

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/right.png"></button>',
         responsive: [
             {
                 breakpoint: 992,
@@ -100,5 +100,21 @@ $(document).ready(function(){
     });
 
     // Smooth scroll and pageup
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1200) {
+        $('.pageup').fadeIn();
+
+      } else {
+        $('.pageup').fadeOut();
+      }
+    });
+
+    // Плавная прокрутка
+    $("a[href^='#']").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+  });
+  new WOW().init();
 });
 
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'vitkagricenko@gmail.com';                 // Наш логин
+$mail->Username = 'vitalik.gricenko77@gmail.com';                 // Наш логин
 $mail->Password = 'http1703';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('vitkagricenko@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('vitalik.gricenko77@gmail.com');     // Add a recipient
+$mail->setFrom('vitalik.gricenko77@gmail.com', 'Pulse');   // От кого письмо 
+$mail->addAddress('vitkagricenko@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
