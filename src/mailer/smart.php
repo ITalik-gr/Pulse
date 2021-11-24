02bc68d6b1d8af871736e51525b72ce7ac31bc2a
ITalik <46629703+ITalik-gr@users.noreply.github.com>
2021-11-24 14:50:30 +0200

    Update smart.php

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'vitalik.gricenko77@gmail.com';                 // Наш логин
-$mail->Password = 'http1703';                           // Наш пароль от ящика
+$mail->Password = 'h';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
